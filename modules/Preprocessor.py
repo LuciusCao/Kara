0d1f28785801186ec5746f1cbd82c5ae7b608cdd
LuciusCao <lucius.cao@gmail.com>
2017-07-21 23:53:12 +0800

    add calc_files_to_convert method so that the code won't do all conversions all the time

--- modules/Preprocessor.py
+++ modules/Preprocessor.py
@@ -8,14 +8,19 @@ class Preprocessor:
             'wav': os.path.abspath(root_path+'/wav')
         }
 
-    def get_list_of_file(self, path, fmt, with_ext=False):
+    def get_list_of_file(self, fmt, with_ext=False):
         ext_len = len(fmt) + 1
         ext = '.%s'%(fmt)
-        files_from_fmt = os.listdir(path[fmt])
+        files_from_fmt = os.listdir(self.path[fmt])
         if with_ext:
             fmt_files = [item for item in files_from_fmt if item[-ext_len:] == ext]
         else:
             fmt_files = [item[:-ext_len] for item in files_from_fmt if itemp[-ext_len:] == ext ]
         return fmt_files
 
+    def calc_files_to_convert(self):
+        wav_files = self.get_list_of_file('wav')
+        mp3_files = self.get_list_of_file('mp3')
+        target_files = [item for item in mp3_files if item not in wav_files]
+        return target_files
 
