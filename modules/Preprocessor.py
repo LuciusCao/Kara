3f66b4ac98a2270b5dae720d4f58278d07a2c129
LuciusCao <lucius.cao@gmail.com>
2017-07-22 21:50:36 +0800

    add convert functions

--- modules/Preprocessor.py
+++ modules/Preprocessor.py
@@ -1,7 +1,8 @@
 import os
+from pipes import quote
 
 class Preprocessor:
-    def __init__(self, root_pathm, sample_rate=44100):
+    def __init__(self, root_path, sample_rate=44100):
         self.path = {
             'mp3': os.path.abspath(root_path+'/mp3'),
             'tmp': os.path.abspath(root_path+'/tmp'),
@@ -22,5 +23,20 @@ class Preprocessor:
         mp3_files = self.get_list_of_file('mp3')
         return [item for item in mp3_files if item not in wav_files]
 
-    def convert_mp3_to_wav(self):
+    def convert_mp3_to_wav(self, filename):
+        mp3_file_path = '%s/%s.mp3'%(path['mp3'], filename)
+        tmp_file_path = '%s/%s.mp3'%(path['tmp'], filename)
+        wav_file_path = '%s/%s.wav'%(path['wav'], filename)
+        cmd = 'lame -a -m m %s %s'%(quote(mp3_file_path), quote(tmp_file_path))
+        os.system(cmd)
+        cmd = 'lame --decode %s %s --resample %s'%(quote(tmp_file_path), quote(wav_file_path), str(self.sample_rate))
+        os.system(cmd)
+        return
 
+    def convert_all(self):
+        i = 0
+        for each_mp3 in self.target_files:
+            self.convert_mp3_to_wav(each_mp3)
+            i += 1
+        print (i, 'files have been converted')
+        return i
