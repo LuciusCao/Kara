050cec23d683ec47da7cc5e04dfba95f1f4367b5
LuciusCao <lucius.cao@gmail.com>
2017-07-23 17:47:29 +0800

    add Loader module to read wav file and perform fourier transform on the music

---
+++ modules/Loader.py
@@ -0,0 +1,31 @@
+import scipy.io.wavfile as wav
+import numpy as np
+
+
+class Loader:
+    def read_wav_to_np(filename):
+        data = wav.read(filename)
+        np_arr = data[1].astype('float32') / 32767.0
+        return np_arr, data[0]
+
+    def convert_np_audio_to_seq(music_arr, seq_length):
+        num_seqs = len(music_arr) // seq_length
+        data = np.zeros((num_seqs, seq_length))
+        cur_pointer = 0
+        while cur_pointer < num_seqs:
+            seq = music_arr[cur_pointer*seq_length:(cur_pointer+1)*seq_length]
+            if seq.shape[0] < seq_length:
+                padding = np.zeros((seq_length - seq.shape[0],))
+                seq = np.concatenate((seq, padding))
+            data[cur_pointer, :] = seq
+            cur_pointer += 1
+        return data
+
+    def fourier_transform(data):
+        row, col = data.shape
+        fft_data = np.zeros((row, col*2))
+        for i in range(row):
+            fft_seq = np.fft.fft(data[i, :])
+            target_seq = np.concatenate((np.real(fft_seq), np.imag(fft_seq)))
+            fft_data[i] = target_seq
+        return fft_data
