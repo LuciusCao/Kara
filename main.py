93609605f94feb4adf33a73de08869104cf83171
LuciusCao <lucius.cao@gmail.com>
2017-07-25 21:26:17 +0800

    add sequence method and refactor code a little bit

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from modules.Preprocessor import Preprocessor
 from modules.Loader import Loader
 from modules.Writer import Writer
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers.wrappers import Bidirectional
+from keras.layers import Activation, Dense
+from keras.layers import Conv1D, MaxPooling1D
 import os
-import helpers as h
+
 
 if __name__ == '__main__':
     root_path = os.path.abspath('./dataset')
@@ -10,9 +15,11 @@ if __name__ == '__main__':
     preprocessor.convert_all()
     loader = Loader(
         os.path.abspath('./dataset/wav/a_hisa - Town of Windmill.wav'),
-        2048
+        2048, 256
     )
+    x, y, shape = loader.load_training_data()
     writer = Writer()
-    # org_seq = writer.de_fourier_transform(loader.fourier_sequences)
-    # data_from_writer = writer.convert_seq_to_np_audio(org_seq)
-    # data_from_loader = writer.convert_seq_to_np_audio(loader.sequences)
+    # experiment code below
+    model = Sequential()
+
+    model.compile(loss='mean_absolute_error', optimizer='rmsprop')
--- modules/Loader.py
+++ modules/Loader.py
@@ -3,44 +3,61 @@ import numpy as np
 
 
 class Loader:
-    def __init__(self, filepath, block_size):
+    def __init__(self, filepath, block_size, max_seq_len):
         self.filepath = filepath
-        self._block_size = block_size
-        self.data, self.sample_rate = self._read_wav_to_np(self.filepath)
-        self.blocks = self._convert_np_audio_to_block(self.data,
-                                                       self._block_size)
-        self.fourier_blocks = self._fourier_transform(self.blocks)
+        self.max_seq_len = max_seq_len
+        self.block_size = block_size
+        self.raw_data, self.sample_rate = self._read_wav_to_np(self.filepath)
 
     def _read_wav_to_np(self, filename):
         data = wav.read(filename)
         np_arr = data[1].astype('float32') / 32767.0
         return np_arr, data[0]
 
-    def _convert_np_audio_to_block(self, music_arr, block_size):
-        num_blocks = len(music_arr) // block_size
-        data = np.zeros((num_blocks, block_size))
+    def _convert_np_audio_to_block(self, music_arr):
+        num_blocks = len(music_arr) // self.block_size
+        data = []
         cur_pointer = 0
         while cur_pointer < num_blocks:
-            block = music_arr[cur_pointer*block_size:(cur_pointer+1)*block_size]
-            if block.shape[0] < block_size:
-                padding = np.zeros((block_size - block.shape[0],))
+            block = music_arr[cur_pointer*self.block_size:
+                              (cur_pointer+1)*self.block_size]
+            if block.shape[0] < self.block_size:
+                padding = np.zeros((self.block_size - block.shape[0],))
                 block = np.concatenate((block, padding))
-            data[cur_pointer, :] = block
+            data.append(block)
             cur_pointer += 1
         return data
 
     def _fourier_transform(self, data):
-        # plausible need confirm
-        row, col = data.shape
-        fft_data = np.zeros((row, col*2))
+        row = len(data)
+        fft_data = []
         for i in range(row):
-            fft_block = np.fft.fft(data[i, :])
+            fft_block = np.fft.fft(data[i])
             target_block = np.concatenate((np.real(fft_block),
                                            np.imag(fft_block)))
-            fft_data[i] = target_block
+            fft_data.append(target_block)
         return fft_data
 
-    def load_training_data(self, data_set):
-        train_x = data_set
-        train_y = train_x[1:]
-        train_y.append(np.zeros(self._block_size))
+    def load_training_data(self):
+        x = self._convert_np_audio_to_block(self.raw_data)
+        y = x[1:]
+        y.append(np.zeros(self.block_size))
+        fft_x = self._fourier_transform(x)
+        fft_y = self._fourier_transform(y)
+        cur_seq = 0
+        total_seq = len(x)
+        train_x = []
+        train_y = []
+        while cur_seq + self.max_seq_len < total_seq:
+            train_x.append(fft_x[cur_seq:cur_seq+self.max_seq_len])
+            train_y.append(fft_y[cur_seq:cur_seq+self.max_seq_len])
+            cur_seq += self.max_seq_len
+        num_examples = len(train_x)
+        num_dims_out = self.block_size * 2
+        shape = (num_examples, self.max_seq_len, num_dims_out)
+        return train_x, train_y, shape
+        # train_x = np.array(train_x)
+        # train_y = np.array(train_y)
+        # mean_x = np.mean(np.mean(train_x, axis=0), axis=0)
+        # std_x = np.sqrt(
+            # np.mean(np.mean((train_x - mean_x) ** 2, axis=0), axis=0))
