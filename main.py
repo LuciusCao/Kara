295467910f97dd58866d0fb6e4f72266c178171b
LuciusCao <lucius.cao@gmail.com>
2017-07-27 11:44:58 +0800

    make the model work by using sequence to sequnce learning. However, Had some problem with the model. seems like it cannot learn from a single song

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from modules.Preprocessor import Preprocessor
 from modules.Loader import Loader
 from modules.Writer import Writer
-from keras.models import Sequential
-from keras.layers.recurrent import LSTM
-from keras.layers.wrappers import Bidirectional
-from keras.layers import Activation, Dense, Flatten
-from keras.layers import Conv1D, MaxPooling1D
+from modules.Model import build_basic, build_td_basic, build_seq2seq
 import os
 
 
@@ -18,10 +14,5 @@ if __name__ == '__main__':
         2048, 32
     )
     x, y, shape = loader.load_training_data()
+    model = build_seq2seq(shape[1], shape[2], 128, depth=3)
     writer = Writer()
-    # experiment code below
-    model = Sequential()
-    model.add(LSTM(4, input_shape=shape[1:]))
-    # model.add(Flatten())
-    # model.add(Dense(2))
-    model.compile(loss='mean_absolute_error', optimizer='rmsprop')
--- modules/Model.py
+++ modules/Model.py
@@ -1,14 +1,29 @@
 from keras.models import Sequential
 from keras.layers.recurrent import LSTM
-from keras.layers.wrappers import Bidirectional
+from keras.layers.wrappers import Bidirectional, TimeDistributed
 from keras.layers import Activation, Dense
+from seq2seq.models import Seq2Seq
 
 
-def build(input_dim):
+def build_seq2seq(time_step, input_dim, hidden_dim, depth=1, batch_size=None):
+    model = Seq2Seq(batch_input_shape=(batch_size, time_step, input_dim),
+                    hidden_dim=hidden_dim,
+                    output_length=time_step,
+                    output_dim=input_dim,
+                    depth=depth)
+    model.compile(loss='mean_absolute_error', optimizer='rmsprop')
+    return model
+
+def build_basic(time_step, input_dim):
     model = Sequential()
-    model.add(Bidirectional(LSTM(16, return_sequences=True),
-                            input_shape=(input_dim, 16)))
-    model.add(Dense(8))
-    model.add(Activation('linear'))
+    model.add(LSTM(16, input_shape=(time_step, input_dim)))
+    model.compile(loss='mean_absolute_error', optimizer='rmsprop')
+    return model
+
+def build_td_basic(time_step, input_dim):
+    model = Sequential()
+    model.add(TimeDistributed(Dense(8, input_shape=(time_step, input_dim))))
+    model.add(LSTM(8, return_sequences=True))
+    model.add(TimeDistributed(Dense(8)))
     model.compile(loss='mean_absolute_error', optimizer='rmsprop')
     return model
