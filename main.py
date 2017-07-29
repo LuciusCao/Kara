f45af87f2bf9af7a4dce76260221885e8b3ded9e
LuciusCao <lucius.cao@gmail.com>
2017-07-29 15:34:30 +0800

    add print message and change some hyperparameter of rmsprop

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ if __name__ == '__main__':
         os.path.abspath('./dataset/wav/a_hisa - Town of Windmill.wav'),
         2048, 32
     )
+    print('========loading data========')
     x, y, shape = loader.load_training_data()
-    model = build_seq2seq(shape[1], shape[2], 128, depth=3)
+    print('========building models========')
+    model = build_seq2seq(shape[1], shape[2], 64, depth=3)
+    print('========training========')
+    model.fit(x, y, batch_size=1, epochs=1)
+    print('========saving model weights========')
+    model.save_weights('trained_weights.h5')
+    print('========Done!========')
     writer = Writer()
--- modules/Model.py
+++ modules/Model.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import Bidirectional, TimeDistributed
 from keras.layers import Activation, Dense
+from keras.optimizers import RMSprop
 from seq2seq.models import Seq2Seq
 
 
@@ -11,7 +12,8 @@ def build_seq2seq(time_step, input_dim, hidden_dim, depth=1, batch_size=None):
                     output_length=time_step,
                     output_dim=input_dim,
                     depth=depth)
-    model.compile(loss='mean_absolute_error', optimizer='rmsprop')
+    optimizer = RMSprop(lr=0.00001, rho=0.9, epsilon=1e-08, decay=0.0)
+    model.compile(loss='mean_absolute_error', optimizer=optimizer)
     return model
 
 def build_basic(time_step, input_dim):
