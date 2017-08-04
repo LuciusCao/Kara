cabd428f0b1448f6f17632020a86e54ea31c4ec3
LuciusCao <lucius.cao@gmail.com>
2017-08-04 11:57:54 +0800

    fixed code style issue

--- config.py
+++ config.py
@@ -9,13 +9,14 @@ input_wav = os.path.join(dataset_root, 'wav')
 
 config = {
     'project_root': project_root,
-    'dataset_root' : dataset_root,
-    'saved_data' : {
-        'x' : saved_data + '_x.npy',
-        'y' : saved_data + '_y.npy',
+    'dataset_root': dataset_root,
+    'saved_data': {
+        'x': saved_data + '_x.npy',
+        'y': saved_data + '_y.npy',
     },
-    'input_wav' : input_wav,
+    'input_wav': input_wav,
     'out_wav': os.path.abspath('./output.wav'),
+    'model': os.path.join(project_root, 'trained_weights.h5'),
     'timestep': 4,
     'seq_len': 2048,
     'epochs': 1,
